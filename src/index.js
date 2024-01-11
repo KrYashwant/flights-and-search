721f94990f993fcd8f0b7e402fedcbae14cf466c
Yashwant Kumar <kumaryashwant3446@gmail.com>
2024-01-11 17:37:23 +0530

    readme file modified for pg library installtion ,fixed some version vulnerability

--- src/index.js
+++ src/index.js
@@ -1,28 +1,30 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const { PORT } = require('./config/serverConfig');
-const ApiRoutes = require('./routes/index');
+const { PORT } = require("./config/serverConfig");
+const ApiRoutes = require("./routes/index");
 
-const db = require('./models/index');
+const db = require("./models/index");
 // const {Airplane} = require('./models/index');
 
 const setupAndStartServer = async () => {
+  // create the express object
+  const app = express();
 
-    // create the express object
-    const app = express();
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
 
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended: true}));
+  app.use("/api", ApiRoutes);
 
-    app.use('/api', ApiRoutes);
-
-    app.listen(PORT, async () => {
-        console.log(`Server started at ${PORT}`);
-        if(process.env.SYNC_DB) {
-            db.sequelize.sync({alter: true});
-        }
-    });
-}
+  app.listen(PORT, async () => {
+    const date = new Date();
+    console.log(
+      `${date.toDateString()} ${date.toTimeString()} Server started at ${PORT}`
+    );
+    if (process.env.SYNC_DB) {
+      db.sequelize.sync({ alter: true });
+    }
+  });
+};
 
 setupAndStartServer();
